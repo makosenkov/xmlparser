d31a8e4aaaa4df447cf50ded86fbd213647b99cb
Mikhail Kosenkov <makosenkov@gmail.com>
2019-07-23 16:19:24 +0300

    все еще попытки найти файлы

--- src/main/java/XMLParser.java
+++ src/main/java/XMLParser.java
@@ -5,13 +5,15 @@ import javax.xml.transform.*;
 import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.*;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Date;
 
 public class XMLParser {
-    private static final String configPAth = "/fop.xml";
-    private static final String templatePath = "/template.xsl";
-    private static final String defaultName = "default.pdf";
+    private static final String configPAth = "fop.xml";
+    private static final String templatePath = "template.xsl";
+    private static final String fontPath = "TIMES_NEW_ROMAN.TTF";
+    private static final String defaultName = "/default.pdf";
 
     public void parse(String inputFileName,
                       String outputFileName) throws IOException {
@@ -22,10 +24,8 @@ public class XMLParser {
             throw new IllegalArgumentException("Неверный входной файл");
         }
 
-        String path = XMLParser.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-        String decodedPath = URLDecoder.decode(path, StandardCharsets.UTF_8);
-        File config = new File(decodedPath + configPAth);
-        File template = new File(decodedPath + templatePath);
+        File config = getResource(configPAth);
+        File template = getResource(templatePath);
 
         try (OutputStream out = new BufferedOutputStream(new FileOutputStream(new File(outputFileName)))) {
             FopFactory fopFactory = FopFactory.newInstance(config);
@@ -36,9 +36,35 @@ public class XMLParser {
             Source src = new StreamSource(new File(inputFileName));
             Result result = new SAXResult(fop.getDefaultHandler());
             transformer.transform(src, result);
-        } catch (TransformerException | SAXException | FileNotFoundException e) {
+        } catch (FileNotFoundException | SAXException | TransformerException e) {
             e.printStackTrace();
         }
+    }
+
+    private File getResource(String resource) {
+        File file = null;
+        URL res = getClass().getResource(resource);
+        if (res.toString().startsWith("jar:")) {
+            try (InputStream input = getClass().getResourceAsStream(resource)){
+                file = Files.createTempFile(new Date().getTime()+"", ".html").toFile();
+                System.out.println(file.getPath());
+                System.out.println(new File(fontPath).getPath());
+                try (OutputStream out = new FileOutputStream(file)){
+                    int read;
+                    byte[] bytes = new byte[1024];
+
+                    while ((read = input.read(bytes)) != -1) {
+                        out.write(bytes, 0, read);
+                    }
+                }
+                file.deleteOnExit();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            file = new File(res.getFile());
+        }
+        return file;
     }
 
 }
